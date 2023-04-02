26aadf9f57885b15a2faae0305a138c2795bcf7c
lucasrosa14 <contact.lucassr@gmail.com>
2023-04-02 10:12:15 -0300

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -39,7 +39,7 @@ public class LoginPage extends RunCucumber {
     }
 
     public void verificaLoginSucesso(){
-        checkMessage((By.id("swal2-title")), "Login realizad");
+        checkMessage((By.id("swal2-title")), "Login realizado");
     }
 
     public void verificaCampoVazio(String message){
