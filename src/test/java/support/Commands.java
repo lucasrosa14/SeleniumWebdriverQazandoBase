cfe902d239f89501d561ab47570824e9fdb9273b
lucasrosa14 <contact.lucassr@gmail.com>
2023-04-12 13:40:49 -0300

    Ajustes Commands

--- src/test/java/support/Commands.java
+++ src/test/java/support/Commands.java
@@ -50,7 +50,7 @@ public class Commands extends RunCucumber {
     }
 
     public static void checkMessage(By element, String expectedMessage) {
-        System.out.println("##############################################");
+        System.out.println("***************************************************");
         System.out.println("********* Vai validar mensagem: " + expectedMessage);
         waitElementBeVisible(element, Duration.ofMillis(2000));
         String actualMessage = getDriver().findElement(element).getText();
@@ -60,7 +60,7 @@ public class Commands extends RunCucumber {
         //Quando não der certo, vai quebrar o teste!!!
 
         System.out.println("********* Validou mensagem: " + expectedMessage);
-        System.out.println("##############################################");
+        System.out.println("***************************************************");
 
     }
 
